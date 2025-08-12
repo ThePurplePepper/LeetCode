6d12d2fe2ea999bb031683ed912ba121142b4b58
ThePurplePepper <yoav.yihyie@gmail.com>
2025-08-12 17:46:25 +0300

    add loop for continuing solving

--- main.cpp
+++ main.cpp
@@ -7,16 +7,34 @@ using namespace std;
 int main() {
     ProblemFactory factory;
     registerAllProblems(factory);
+    bool exit = true;
+    string exitStatus;
 
-    unsigned int id;
-    cout << "Enter problem ID" << endl;
-    cin >> id;
+    while (exit) {
+        unsigned int id = 0;
+        cout << "Enter problem ID" << endl;
+        cin >> id;
 
-    auto problem = factory.createProblem(id);
-    if (problem) {
+        auto problem = factory.createProblem(id);
+        if (problem) {
+            cout << "";
+        } else {
+            cout << "Problem not solved yet" << endl;
+        }
 
-    } else {
-        cout << "Problem not solved yet" << endl;
+        do {
+            cout << "Continue? [Y/N]" << endl;
+            cin >> exitStatus;
+            if (exitStatus == "n" || exitStatus == "N") {
+                cout << "See you next time!" << endl;
+                exit = false;
+                break;
+            } if (exitStatus != "y" && exitStatus != "Y") {
+                cout << "I didn't quite catch that..." << endl;
+            }
+        } while (exitStatus != "y" && exitStatus != "Y");
+
     }
+
     return 0;
 }
